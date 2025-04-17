4ebba8f610be1ae333d3e00205ccdbb381e31b03
Daniel Mendoza <danielmendoza@MacBookPro.attlocal.net>
2025-04-17 00:39:11 -0500

    rename draft env var

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -5,7 +5,7 @@ import RichText from "@/components/RichText";
 //Content is memoized - any fetch requests with the same arguments will not be made again
 export const generateStaticParams = async () => {
 	//await our response from the api endpoint
-	const data = await fetch(`https://api.storyblok.com/v2/cdn/stories?content_type=blog&version=${process.env.NODE_ENV === "development" ? "draft" : "published"}&token=${process.env.STORYBLOCK_PREVIEW_ACCESS_TOKEN}`)
+	const data = await fetch(`https://api.storyblok.com/v2/cdn/stories?content_type=blog&version=${process.env.DRAFT_MODE === "true" ? "draft" : "published"}&token=${process.env.STORYBLOCK_PREVIEW_ACCESS_TOKEN}`)
 	//await the response body to be parsed as Json
 	const response = await data.json();
 	return response.stories.map((story: { slug: string }) => ({
@@ -18,7 +18,7 @@ const fetchPage = async (slug: string) => {
 	//we would then need to handle turning preview mode on and off - we would do that by adding a preview route handler in the api folder
 	// We could use the Storyblok sdk here, but we are leveraging on Nextjs fetch api
 	//Since we prefetched the routes above, and our arguments are the same here, we will be statically rendering these pages
-	const data = await fetch(`https://api.storyblok.com/v2/cdn/stories/${slug}?version=${process.env.NODE_ENV === "development" ? "draft" : "published"}&token=${process.env.STORYBLOCK_PREVIEW_ACCESS_TOKEN}`)
+	const data = await fetch(`https://api.storyblok.com/v2/cdn/stories/${slug}?version=${process.env.DRAFT_MODE === "true" ? "draft" : "published"}&token=${process.env.STORYBLOCK_PREVIEW_ACCESS_TOKEN}`)
 	const response = await data.json();
 	return response.story;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   console.log('fetch')
 	// We could use the Storyblok sdk here, but we are leveraging on Nextjs fetch api
 	//Since we prefetched the routes above, and our arguments are the same here, we will be statically rendering these pages
-	const data = await fetch(`https://api.storyblok.com/v2/cdn/stories?content_type=blog&version=${process.env.NODE_ENV === "development" ? "draft" : "published"}&token=${process.env.STORYBLOCK_PREVIEW_ACCESS_TOKEN}`)
+	const data = await fetch(`https://api.storyblok.com/v2/cdn/stories?content_type=blog&version=${process.env.DRAFT_MODE === "true" ? "draft" : "published"}&token=${process.env.STORYBLOCK_PREVIEW_ACCESS_TOKEN}`)
 	const response = await data.json();
 
   return <div>
